a49074b71c2614f2e6a215c59b276da1c7f42471
Federico Melo <federico@conexa.ai>
2022-06-18 02:53:32 -0300

    Added validation in AuthController.java for login

--- src/main/java/com/melof10/auth/security/controllers/AuthController.java
+++ src/main/java/com/melof10/auth/security/controllers/AuthController.java
@@ -85,24 +85,24 @@ public class AuthController {
         if(bindingResult.hasErrors())
             return new ResponseEntity(new Message("Invalid fields or email"), HttpStatus.BAD_REQUEST);
 
-        if(loginUserDTO.getEmail() != null) {
+        if(loginUserDTO.getEmail() != null && loginUserDTO.getUsername() == null) {
             user = iUserService.getByEmail(loginUserDTO.getEmail());
-            if(!iUserService.existsByEmail(loginUserDTO.getEmail()) || !passwordEncoder.matches(loginUserDTO.getPassword(), user.get().getPassword())) {
+
+            if(!iUserService.existsByEmail(loginUserDTO.getEmail()) || !passwordEncoder.matches(loginUserDTO.getPassword(), user.get().getPassword()))
                 return new ResponseEntity(new Message("Email or Password incorrect"), HttpStatus.BAD_REQUEST);
-            } else {
+            else
                 authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUserDTO.getEmail(), loginUserDTO.getPassword()));
-            }
         } else if(loginUserDTO.getEmail() == null && loginUserDTO.getUsername() == null) {
             return new ResponseEntity(new Message("Email required"), HttpStatus.BAD_REQUEST);
         }
 
-        if(loginUserDTO.getUsername() != null) {
+        if(loginUserDTO.getUsername() != null && loginUserDTO.getEmail() == null) {
             user = iUserService.getByUsername(loginUserDTO.getUsername());
-            if(!iUserService.existsByUsername(loginUserDTO.getUsername()) || !passwordEncoder.matches(loginUserDTO.getPassword(), user.get().getPassword())) {
+
+            if(!iUserService.existsByUsername(loginUserDTO.getUsername()) || !passwordEncoder.matches(loginUserDTO.getPassword(), user.get().getPassword()))
                 return new ResponseEntity(new Message("Username or Password incorrect"), HttpStatus.BAD_REQUEST);
-            } else {
+            else
                 authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUserDTO.getUsername(), loginUserDTO.getPassword()));
-            }
         } else if(loginUserDTO.getEmail() == null && loginUserDTO.getUsername() == null){
             return new ResponseEntity(new Message("Username required"), HttpStatus.BAD_REQUEST);
         }
